ea1b7ce45e3b6e7cddd1d75a847db400fd9f4ee2
btbruno <brunommores@gmail.com>
2025-02-26 20:30:21 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -20,8 +20,6 @@ st.checkbox('Refrigerante')
 st.color_picker("Pick a Color", "#00f900")
 st.feedback("stars")
 
-st.ballons("")
-
 df = pd.DataFrame(
   [
     {"command": "st.selectbox", "rating":4, "is_widget":True},
@@ -30,3 +28,38 @@ df = pd.DataFrame(
   ]
 )
 edited_df = st.data_editor(df)
+
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+from streamlit.proto.Balloons_pb2 import Balloons as BalloonsProto
+from streamlit.runtime.metrics_util import gather_metrics
+
+if TYPE_CHECKING:
+    from streamlit.delta_generator import DeltaGenerator
+
+
+class BalloonsMixin:
+    @gather_metrics("balloons")
+    def balloons(self) -> DeltaGenerator:
+        """Draw celebratory balloons.
+
+        Example
+        -------
+        >>> import streamlit as st
+        >>>
+        >>> st.balloons()
+
+        ...then watch your app and get ready for a celebration!
+
+        """
+        balloons_proto = BalloonsProto()
+        balloons_proto.show = True
+        return self.dg._enqueue("balloons", balloons_proto)
+
+    @property
+    def dg(self) -> DeltaGenerator:
+        """Get our DeltaGenerator."""
+        return cast("DeltaGenerator", self)
