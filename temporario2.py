8b64a6527220a08d8f3d280ad9cce5447fcacead
btbruno <brunommores@gmail.com>
2025-02-28 20:16:30 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,9 +1,9 @@
 import datetime
 import streamlit as st
+print("Aluno")
+st.text_input("Nome:",key="i_1" max_chars=50)
 
-st.text_input("Nome:",key=i_1 max_chars=50)
-
-st.text_input("CPF:",key=i_2 max_chars=11)
+st.text_input("CPF:",key="i_2" max_chars=11)
 
 d = st.date_input("Data de nascimento", value=None)
 if d:
