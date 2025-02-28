96afc3d5afae5e7cd759ffd9c9382e6400cefcd5
btbruno <brunommores@gmail.com>
2025-02-28 20:12:05 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 st.text_input("Nome:", max_chars=50)
 
+st.text_input("CPF:", max_chars=11)
+
 d = st.date_input("Data de nascimento", value=None)
 if d:
     st.write("Data selecionada:", d.strftime("%d/%m/%Y"))
