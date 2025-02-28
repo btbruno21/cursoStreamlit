6ad4cc18c1eb7e917c8313aa0e9c18803e48eb64
btbruno <brunommores@gmail.com>
2025-02-28 20:07:08 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,6 +1,8 @@
 import datetime
 import streamlit as st
 
+st.text_area("Nome:")
+
 d = st.date_input("Data de nascimento", value=None)
 if d:
     st.write("Data selecionada:", d.strftime("%d/%m/%Y"))
