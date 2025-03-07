102168d41a64617c9a9b46a275ac54f6741a30fc
btbruno <brunommores@gmail.com>
2025-03-07 20:22:31 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -34,4 +34,4 @@ edited_df = st.data_editor(df)
 if st.button("Balões"):
     st.balloons()
 
-st.sidebar.radio("Escolha a operação", ("Cadastrar Cliente", "Cadastrar Pedido", "Visualizar Registros"))
+st.sidebar("Escolha a operação", ("Cadastrar Cliente", "Cadastrar Pedido", "Visualizar Registros"))
