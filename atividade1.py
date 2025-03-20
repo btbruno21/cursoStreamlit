71c3ab7c7ba2ace2b5427923ee9f540bbfc0f31e
btbruno <brunommores@gmail.com>
2025-03-20 14:44:19 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -3,11 +3,11 @@ import streamlit as st
 with st.container():
     st.header("Ingredientes para o Bolo de Leite Condensado")
     
-    latas_leite_condensado = float(st.text_input("Quantidade de latas de leite condensado necessárias para a receita:"))
-    gramas_por_lata = float(st.text_input("Digite a quantidade de gramas que cada lata de leite condensado tem:"))
-    quantidade_xicaras_f = float(st.text_input("Quantidade de xícaras de farinha necessárias para a receita:"))
-    gramas_por_xicara_f = float(st.text_input("Digite a quantidade de gramas que cada xícara de farinha tem:"))
-    quantidade_ovos = float(st.text_input("Quantidade de ovos necessários para a receita:"))
+    latas_leite_condensado = float(st.text_input("Quantidade de latas de leite condensado necessárias para a receita:","0.0"))
+    gramas_por_lata = float(st.text_input("Digite a quantidade de gramas que cada lata de leite condensado tem:","0.0"))
+    quantidade_xicaras_f = float(st.text_input("Quantidade de xícaras de farinha necessárias para a receita:","0.0"))
+    gramas_por_xicara_f = float(st.text_input("Digite a quantidade de gramas que cada xícara de farinha tem:","0.0"))
+    quantidade_ovos = float(st.text_input("Quantidade de ovos necessários para a receita:","0.0"))
 
     st.subheader("Ingredientes recebidos:")
     st.write(f"Latas de leite condensado: {latas_leite_condensado} latas ({latas_leite_condensado * gramas_por_lata}g)")
